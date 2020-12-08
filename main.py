878e0e948530a75d732beb0a6d7737d8eab470dd
DawidKosior <d4wt0n@gmail.com>
2020-12-08 14:06:46 +0100

    first push

---
+++ main.py
@@ -0,0 +1,43 @@
+"""
+Simulating of the Lattice Gas Automata
+-----------------------------
+=====================================================
+
+This application is my project for Discrete Modeling.
+Dawid Kosior, Computational Engineering
+
+=====================================================
+"""
+
+import tkinter as tk
+import cv2
+import numpy as np
+import random
+import sys
+import os
+
+"""
+Functions
+---------
+============================================
+
+============================================
+"""
+
+"""
+GUI
+---
+=========================
+
+=========================
+
+
+Methods
+-------
+=========================================================
+
+=========================================================
+"""
+
+if __name__ == '__main__':
+    pass
